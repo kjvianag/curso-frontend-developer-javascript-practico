fea51f2ba5e4c7ef18fc915c61247ee1c8d33a0c
Kelly <kjvianagomez@gmail.com>
2022-08-04 20:52:27 -0500

    clase 20

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const btnMobileMenu = document.querySelector('.movil-menu');
+const btnMobileMenu = document.querySelector('.btnMobileMenu');
 const MobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.shoppingCart');
+const productDetail = document.querySelector('.product-detail');
 
 
 navbarEmail.addEventListener('click',toggleDesktopMenu);
 btnMobileMenu.addEventListener('click',toggleMobileMenu);
+shoppingCart.addEventListener('click',toggleProductDetail);
 
 function toggleDesktopMenu() {
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+    if(!isProductDetailClosed){
+        productDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+    if(!isProductDetailClosed){
+        productDetail.classList.add('inactive');
+    }
+
     MobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = MobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        MobileMenu.classList.add('inactive');
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    productDetail.classList.toggle('inactive');
 }
