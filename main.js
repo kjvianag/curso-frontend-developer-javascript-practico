9c739a6500f8a4e80e3763c075dc23b4704f64f6
Kelly <kjvianagomez@gmail.com>
2022-08-04 19:56:32 -0500

    clase 19

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnMobileMenu = document.querySelector('.movil-menu');
+const MobileMenu = document.querySelector('.mobile-menu');
+
 
 navbarEmail.addEventListener('click',toggleDesktopMenu);
+btnMobileMenu.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    MobileMenu.classList.toggle('inactive');
 }
